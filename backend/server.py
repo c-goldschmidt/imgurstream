e16469b102dcb4249a8d91976ad06eef91bd7928
ConradG <congold@live.de>
2016-04-02 14:37:58 +0200

    added better path handling (i was accidentally sending favicon requests :P)

--- backend/server.py
+++ backend/server.py
@@ -7,25 +7,37 @@ class HttpReqHandler(BaseHTTPRequestHandler):
 		self.send_response(200)
 		self.send_header("Content-type", "application/json")
 		self.end_headers()
+		
+	def _get_params_from_path(self):
+		path = self.path.split('/')[1:]
+		
+		gallery_ok = ('hot', 'top', 'user')
+		sort_ok = ('viral', 'top', 'time')
+		
+		gallery = 'user'
+		sort = 'time'
+		showViral = False
+		
+		try:
+			if path[0] in gallery_ok:
+				gallery = path[0]
+				
+			if path[1] in sort_ok:
+				sort_ok = path[1]
+				
+			showViral = path[2] == 'true'
+		except IndexError:
+			pass
+			
+		return gallery, sort, showViral
 	
 	def do_GET(self):
 		imgur = ImgurAPI()
 		
 		self.do_HEAD()
 		
-		path = self.path.split('/')[1:]
+		gallery, sort, showViral = self._get_params_from_path()
 				
-		gallery = 'user'
-		sort = 'time'
-		showViral = False
-		
-		try:
-			gallery = path[0] if not path[0] == '' else 'user'
-			sort = path[1] if not path[1] == '' else 'time'
-			showViral = path[2] == 'true'
-		except IndexError:
-			pass
-		
 		ret = {
 			'path': self.path,
 			'url': imgur.query_newest_image(gallery, sort, showViral)
